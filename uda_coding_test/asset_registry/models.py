a065c4c6355cfcf22109adb02b36fcfc1e596a44
garcel <garcel.developer@gmail.com>
2020-11-28 12:10:46 +0100

    created asset_registry models and performed migrations

--- uda_coding_test/asset_registry/models.py
+++ uda_coding_test/asset_registry/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class GeoPosition(models.Model):
+    """Model representing the coordinates of a terrestrial point."""
+
+    # Fields
+    latitude = models.DecimalField(max_digits=13, decimal_places=10)
+    longitude = models.DecimalField(max_digits=13, decimal_places=10)
+
+    # Methods
+    def __str__(self):
+        return f'{self.latitude}, {self.longitude}'
+
+
+class Asset(models.Model):
+    """Model representing an asset."""
+
+    # Fields
+    # Mandatory
+    geom = models.OneToOneField(GeoPosition, on_delete=models.CASCADE)
+    area = models.IntegerField()
+
+    # Optional
+    address = models.CharField(max_length=60, blank=True)
+    garage = models.BooleanField(blank=True)
+    other = models.JSONField(blank=True)
+    rooms = models.IntegerField(blank=True)
+
+    # Methods
+    def __str__(self):
+        return f'{self.geom}'
--- uda_coding_test/settings.py
+++ uda_coding_test/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from uda_coding_test import asset_registry
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'uda_coding_test.asset_registry',
 ]
 
 MIDDLEWARE = [
